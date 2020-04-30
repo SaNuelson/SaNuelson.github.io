3064b69838c07ec4e579fe09fd026214f0c59489
sanuel <sanuel@Nitro5SB>
2020-04-30 20:35:43 +0200

    fixed popup once again

--- stastnyzivot/web/js/image-popup.js
+++ stastnyzivot/web/js/image-popup.js
@@ -1,11 +1,15 @@
-$(window).on("load hashchange",() => {
+$(window).on("load hashchange",function(){
   // check for popup
   if (window.location.href.includes("#")) {
     let imgname = window.location.href.match(/\#.*/)[0].substr(1);
     $("img").each((_, img) => {
       if (img.src.includes(imgname))
-        $(img).trigger('click');
+        loadModal(img,false);
     })
+  }
+  else
+  {
+    modal.style.display = 'none';
   }
 })
 
@@ -22,8 +26,6 @@ span.onclick = function () {
 
 $(document).keydown(function (e) { if (e.key === "Escape") tryHidePopup(); })
 
-$(window).on('popstate', function () { tryHidePopup(false) });
-
 function tryHidePopup(goBack = true) {
   if (modal.style.display !== "none") {
     if (window.history && goBack)
@@ -34,12 +36,15 @@ function tryHidePopup(goBack = true) {
 
 var imgs = [...document.getElementsByClassName('popup-img')];
 imgs.forEach((img) => {
-  img.addEventListener('click', function () {
-    console.log(img);
-    modal.style.display = 'block';
-    modalImg.src = img.src.replace("-thumbnail.jpg", ".jpg");
-    captionText.innerHTML = img.alt;
-    var imgname = modalImg.src.replace(/^.*[\\\/]/, '').replace(/\.[^.]*/, "");
-    window.history.pushState('forward', null, window.location.href.replace(/\#.*/, "") + "#" + imgname);
-  })
+  img.addEventListener('click', function(){loadModal(img);});
 })
+
+function loadModal(img, pushState = true) {
+  console.log(img);
+  modal.style.display = 'block';
+  modalImg.src = img.src.replace("-thumbnail.jpg", ".jpg");
+  captionText.innerHTML = img.alt;
+  var imgname = modalImg.src.replace(/^.*[\\\/]/, '').replace(/\.[^.]*/, "");
+  if(pushState)
+    window.history.pushState('forward', null, window.location.href.replace(/\#.*/, "") + "#" + imgname);
+}
