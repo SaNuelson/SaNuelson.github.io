43ef53d8642bac536f25c4a2778354fb3208cece
sanuel <sanuel@Nitro5SB>
2020-04-30 21:23:41 +0200

    more fix because I'm even dumber

--- stastnyzivot/web/js/custom.js
+++ stastnyzivot/web/js/custom.js
@@ -5,29 +5,29 @@ $("#navToggleButton").click(function(){
     $("#mainWrapper").toggle();
 });
 
+let isPortrait;
+
 // undo hidden state of button when resizing to bigger screen
 $(window).ready(function() {
 
     if (window.innerWidth >= 768) {
-        // main displayed by default
-        // nav wrapper uncollapsed by default
-        // nav bar displayed by default
-        console.log("Load over 768");
+        isPortrait = false;
     }
     else {
-        // main displayed by default
+        isPortrait = true;
         $("#navWrapper").toggleClass("sidebar-collapsed");
-        console.log("Load under 768.");
     }
 });
 
 $(window).resize(function() {
-    if (window.innerWidth >= 768) { // width - no scollbar, outerWidth - scrollbar + window frame, innerWidth - with scrollbar, no window frame.
+    if (window.innerWidth >= 768 && isPortrait) { // width - no scollbar, outerWidth - scrollbar + window frame, innerWidth - with scrollbar, no window frame.
+        isPortrait = false;
         $("#mainWrapper").show();
         $("#navWrapper").removeClass("sidebar-collapsed");
     }
-    else {
-        $("#navWrapper").removeClass("sidebar-collapsed");
+    else if(window.innerWidth < 768 && !isPortrait) {
+        isPortrait = true;
+        $("#navWrapper").addClass("sidebar-collapsed");
         $("#mainWrapper").show();
     }
 });
