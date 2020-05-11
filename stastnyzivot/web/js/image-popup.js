d7775afe6ae9717de3b71667f905cc48455bea60
sanuel <sanuel@Nitro5SB>
2020-05-11 19:30:03 +0200

    added IE11 compatibility

--- stastnyzivot/web/js/image-popup.js
+++ stastnyzivot/web/js/image-popup.js
@@ -1,16 +1,14 @@
-$(window).on("load hashchange",function(){
-  // check for popup
-  if (window.location.href.includes("#")) {
-    let imgname = window.location.href.match(/\#.*/)[0].substr(1);
-    $("img").each((_, img) => {
-      if (img.src.includes(imgname))
-        loadModal(img,false);
-    })
-  }
-  else
-  {
-    modal.style.display = 'none';
-  }
+$(window).on("load hashchange", function() {
+    // check for popup
+    if (window.location.href.search("#") !== -1) {
+        let imgname = window.location.href.match(/\#.*/)[0].substr(1);
+        $("img").each(function(_, img) {
+            if (img.src.includes(imgname))
+                loadModal(img, false);
+        })
+    } else {
+        modal.style.display = 'none';
+    }
 })
 
 // Get the modal
@@ -20,31 +18,35 @@ var captionText = document.getElementById("modalImgCaption");
 var span = document.getElementById("imgPopupCloseBtn");
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  tryHidePopup();
+span.onclick = function() {
+    tryHidePopup();
 }
 
-$(document).keydown(function (e) { if (e.key === "Escape") tryHidePopup(); })
+$(document).keydown(function(e) { if (e.key === "Escape") tryHidePopup(); })
 
-function tryHidePopup(goBack = true) {
-  if (modal.style.display !== "none") {
-    if (window.history && goBack)
-      window.history.back();
-    modal.style.display = "none";
-  }
+function tryHidePopup(goBack) {
+    if (!goBack) goBack = true;
+    if (modal.style.display !== "none") {
+        if (window.history && goBack)
+            window.history.back();
+        modal.style.display = "none";
+    }
 }
 
-var imgs = [...document.getElementsByClassName('popup-img')];
-imgs.forEach((img) => {
-  img.addEventListener('click', function(){loadModal(img);});
-})
+var imgs = document.getElementsByClassName('popup-img');
+// IE11 Compatibility
+for (let i = 0; i < imgs.length; i++) {
+    imgs[i].addEventListener('click', function() { loadModal(this); });
+}
 
-function loadModal(img, pushState = true) {
-  console.log(img);
-  modal.style.display = 'block';
-  modalImg.src = img.src.replace("-thumbnail.jpg", ".jpg");
-  captionText.innerHTML = img.alt;
-  var imgname = modalImg.src.replace(/^.*[\\\/]/, '').replace(/\.[^.]*/, "");
-  if(pushState)
-    window.history.pushState('forward', null, window.location.href.replace(/\#.*/, "") + "#" + imgname);
+function loadModal(img, pushState) {
+    console.log("loadModal: ", img);
+    if (!pushState) pushState = true;
+    console.log(img);
+    modal.style.display = 'block';
+    modalImg.src = img.src.replace("-thumbnail.jpg", ".jpg");
+    captionText.innerHTML = img.alt;
+    var imgname = modalImg.src.replace(/^.*[\\\/]/, '').replace(/\.[^.]*/, "");
+    if (pushState)
+        window.history.pushState('forward', null, window.location.href.replace(/\#.*/, "") + "#" + imgname);
 }
